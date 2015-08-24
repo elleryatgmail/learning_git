1e92e32e1324e007f786b50d5245d51b20e514cb
Ellery Coleman <ellery.coleman@gmail.com>
2015-08-24 19:32:56 +0000

    Updated the file header section for prog.c with standard C header template.

--- prog.c
+++ prog.c
@@ -1,4 +1,9 @@
-/* first version of program */
+/*---------------------------------------------------------
+ | File:	prog.c
+ | Date:	Mon Aug 24 19:30:28 GMT 2015
+ | Author:	ellery coleman
+ | Abstract: 	simple C program to illustrate use of git.
+ +---------------------------------------------------------*/
 
 
 #include <stdio.h>
